a8491b3b7a6e04a6f00f9ee74ad5c98838b45c9a
agudlc <87042363+agudlc@users.noreply.github.com>
2022-05-28 22:04:16 -0300

    added structure of components

---
+++ client/src/components/Register.jsx
@@ -0,0 +1,27 @@
+
+
+export default function Register () {
+    <main>
+        <form action="" method="post">
+            <h1>Register</h1>
+            <section>
+                <fieldset>
+                    <label htmlFor=""></label>
+                    <input type="text" />
+                </fieldset>
+                <fieldset>
+                    <label htmlFor=""></label>
+                    <input type="text" />
+                </fieldset>
+                <fieldset>
+                    <label htmlFor=""></label>
+                    <input type="text" />
+                </fieldset>
+                <fieldset>
+                    <label htmlFor=""></label>
+                    <input type="text" />
+                </fieldset>
+            </section>
+        </form>
+    </main>
+}
